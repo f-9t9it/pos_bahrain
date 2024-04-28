cd25a140abfb9619af2b9dac1c26bfcfd27cf833
Usman <u.khalid@9t9it.com>
2024-04-28 17:36:25 +0300

    sales register for incentive fixes

--- pos_bahrain/pos_bahrain/report/sales_register_for_incentive/sales_register_for_incentive.py
+++ pos_bahrain/pos_bahrain/report/sales_register_for_incentive/sales_register_for_incentive.py
@@ -20,6 +20,11 @@ from frappe.utils import flt
 from frappe import msgprint, _
 from frappe.model.meta import get_field_precision
 from erpnext.accounts.doctype.accounting_dimension.accounting_dimension import get_accounting_dimensions, get_dimension_with_children
+from datetime import datetime
+from functools import partial, reduce
+from toolz import groupby, pluck, compose, merge, keyfilter
+import pandas as pd
+from collections import defaultdict
 
 def execute(filters=None):
 	return _execute(filters)
@@ -28,7 +33,7 @@ def _execute(filters, additional_table_columns=None, additional_query_columns=No
 	if not filters: filters = frappe._dict({})
 
 	invoice_list = get_invoices(filters, additional_query_columns)
-	columns, income_accounts, tax_accounts = get_columns(invoice_list, additional_table_columns)
+	columns, income_accounts, tax_accounts = get_columns(invoice_list, additional_table_columns,filters)
 
 	if not invoice_list:
 		msgprint(_("No record found"))
@@ -128,8 +133,10 @@ def _execute(filters, additional_table_columns=None, additional_query_columns=No
 			for returned_inv_name in returned_invoices:
 				returned_inv = next((invobj for invobj in invoice_list if invobj.name == returned_inv_name), None)
 				if returned_inv:
-					returned_amount_value += returned_inv.base_grand_total
-			remaining_amount = (base_net_total or inv.base_net_total) - (returned_amount_value*-1)
+					# returned_amount_value += returned_inv.base_grand_total
+					returned_amount_value += returned_inv.base_net_total or returned_inv.base_net_total
+			remaining_amount = (base_net_total or inv.base_net_total) - abs(returned_amount_value)
+			returned_amount_value = abs(returned_amount_value)
 			row.update({
 				'returned_amount': returned_amount_value,
 				'remaining_amount': remaining_amount,
@@ -143,46 +150,108 @@ def _execute(filters, additional_table_columns=None, additional_query_columns=No
 			})
 			data.append(row)
 
+ 
+
+	if filters.get('summary_view'):
+		grouped_data = defaultdict(dict)
+		for row in data:
+			sales_team_name = row.get('sales_team_name')
+			if sales_team_name not in grouped_data:
+				# Initialize the fields as lists
+				grouped_data[sales_team_name] = {
+					'invoice': [row['invoice']],
+					'posting_date': [str(row['posting_date'])],
+					'customer': [row['customer']],
+					'customer_name': [row['customer_name']],
+					'pb_sales_employee_name': [row['pb_sales_employee_name']],
+					'return_against': [row['return_against']],
+					'cost_center': [row['cost_center']],
+					'warehouse': [row['warehouse']],
+					'sales___wcpw': row['sales___wcpw'],
+					'net_total': row['net_total'],
+					'vat___wcpw': row['vat___wcpw'],
+					'tax_total': row['tax_total'],
+					'grand_total': row['grand_total'],
+					'rounded_total': row['rounded_total'],
+					'outstanding_amount': row['outstanding_amount'],
+					'amount_before_discount': row['amount_before_discount'],
+					'discount_value': row['discount_value'],
+					'returned_amount': row['returned_amount'],
+					'remaining_amount': row['remaining_amount'],
+					'incentive_value': row['incentive_value'],
+					'sales_team_name' :  row['sales_team_name'],
+				}
+			else:
+				# Append values to the existing fields
+				grouped_data[sales_team_name]['invoice'].append(row['invoice'])
+				grouped_data[sales_team_name]['posting_date'].append(str(row['posting_date']))
+				grouped_data[sales_team_name]['customer'].append(row['customer'])
+				grouped_data[sales_team_name]['customer_name'].append(row['customer_name'])
+				grouped_data[sales_team_name]['pb_sales_employee_name'].append(row['pb_sales_employee_name'])
+				grouped_data[sales_team_name]['return_against'].append(row['return_against'])
+				grouped_data[sales_team_name]['cost_center'].append(row['cost_center'])
+				grouped_data[sales_team_name]['warehouse'].append(row['warehouse'])
+
+				# Sum the numeric fields
+				grouped_data[sales_team_name]['sales___wcpw'] += row['sales___wcpw']
+				grouped_data[sales_team_name]['net_total'] += row['net_total']
+				grouped_data[sales_team_name]['vat___wcpw'] += row['vat___wcpw']
+				grouped_data[sales_team_name]['tax_total'] += row['tax_total']
+				grouped_data[sales_team_name]['grand_total'] += row['grand_total']
+				grouped_data[sales_team_name]['rounded_total'] += row['rounded_total']
+				grouped_data[sales_team_name]['outstanding_amount'] += row['outstanding_amount']
+				grouped_data[sales_team_name]['amount_before_discount'] += row['amount_before_discount']
+				grouped_data[sales_team_name]['discount_value'] += row['discount_value']
+				grouped_data[sales_team_name]['returned_amount'] += row['returned_amount']
+				grouped_data[sales_team_name]['remaining_amount'] += row['remaining_amount']
+				grouped_data[sales_team_name]['incentive_value'] += row['incentive_value']
+		data = list(grouped_data.values())
+ 
+     
+	
+ 
 	return columns, data
 
 
-def get_columns(invoice_list, additional_table_columns):
+def get_columns(invoice_list, additional_table_columns,filters):
 	"""return columns based on filters"""
-	columns = [
-		{
-			'label': _("Invoice"),
-			'fieldname': 'invoice',
-			'fieldtype': 'Link',
-			'options': 'Sales Invoice',
-			'width': 120
-		},
-		{
-			'label': _("Posting Date"),
-			'fieldname': 'posting_date',
-			'fieldtype': 'Date',
-			'width': 80
-		},
-		{
-			'label': _("Customer"),
-			'fieldname': 'customer',
-			'fieldtype': 'Link',
-			'options': 'Customer',
-			'width': 120
-		},
-		{
-			'label': _("Customer Name"),
-			'fieldname': 'customer_name',
-			'fieldtype': 'Data',
-			'width': 120
-		},
-		{
-			'label': _("Sales Employee Name"),
-			'fieldname': 'pb_sales_employee_name',
-			'fieldtype': 'Data',
-			'width': 120
-		}
-	]
-
+	columns = []
+	if not filters.get('summary_view'):
+		columns+=[
+			{
+				'label': _("Invoice"),
+				'fieldname': 'invoice',
+				'fieldtype': 'Link',
+				'options': 'Sales Invoice',
+				'width': 120
+			},
+			{
+				'label': _("Posting Date"),
+				'fieldname': 'posting_date',
+				'fieldtype': 'Date',
+				'width': 80
+			},
+			{
+				'label': _("Customer"),
+				'fieldname': 'customer',
+				'fieldtype': 'Link',
+				'options': 'Customer',
+				'width': 120
+			},
+			{
+				'label': _("Customer Name"),
+				'fieldname': 'customer_name',
+				'fieldtype': 'Data',
+				'width': 120
+			},
+			{
+				'label': _("Sales Employee Name"),
+				'fieldname': 'pb_sales_employee_name',
+				'fieldtype': 'Data',
+				'width': 120
+			}
+		]
+	
 	if additional_table_columns:
 		columns += additional_table_columns
 
@@ -201,6 +270,9 @@ def get_columns(invoice_list, additional_table_columns):
 			"options": 'currency',
 			"width": 120
 		},
+	]
+	if not filters.get('summary_view'):
+		columns +=[
   		{
 			'label': _("Return Against Sales Invoice"),
 			'fieldname': 'return_against',
@@ -208,7 +280,10 @@ def get_columns(invoice_list, additional_table_columns):
 			# 'options': 'Sales Invoice',
 			'width': 120
 		},
-		{
+		]
+		
+	columns +=[
+  		{
 			"label": _("Value without Discount"),
 			"fieldname": "amount_before_discount",
 			"fieldtype": "Currency",
@@ -243,6 +318,9 @@ def get_columns(invoice_list, additional_table_columns):
 			"options": 'currency',
 			"width": 120
 		},
+	]
+	if not filters.get('summary_view'):
+		columns +=[
 		{
 			'label': _("Tax Id"),
 			'fieldname': 'tax_id',
@@ -396,27 +474,27 @@ def get_conditions(filters):
 
 	if filters.get("mode_of_payment"):
 		conditions += """ and exists(select name from `tabSales Invoice Payment`
-			 where parent=`tabSales Invoice`.name
+			 where parent=i.name
 			 	and ifnull(`tabSales Invoice Payment`.mode_of_payment, '') = %(mode_of_payment)s)"""
 
 	if filters.get("cost_center"):
 		conditions +=  """ and exists(select name from `tabSales Invoice Item`
-			 where parent=`tabSales Invoice`.name
+			 where parent=i.name
 			 	and ifnull(`tabSales Invoice Item`.cost_center, '') = %(cost_center)s)"""
 
 	if filters.get("warehouse"):
 		conditions +=  """ and exists(select name from `tabSales Invoice Item`
-			 where parent=`tabSales Invoice`.name
+			 where parent=i.name
 			 	and ifnull(`tabSales Invoice Item`.warehouse, '') = %(warehouse)s)"""
 
 	if filters.get("brand"):
 		conditions +=  """ and exists(select name from `tabSales Invoice Item`
-			 where parent=`tabSales Invoice`.name
+			 where parent=i.name
 			 	and ifnull(`tabSales Invoice Item`.brand, '') = %(brand)s)"""
 
 	if filters.get("item_group"):
 		conditions +=  """ and exists(select name from `tabSales Invoice Item`
-			 where parent=`tabSales Invoice`.name
+			 where parent=i.name
 			 	and ifnull(`tabSales Invoice Item`.item_group, '') = %(item_group)s)"""
 
 	accounting_dimensions = get_accounting_dimensions(as_list=False)
@@ -424,7 +502,7 @@ def get_conditions(filters):
 	if accounting_dimensions:
 		common_condition = """
 			and exists(select name from `tabSales Invoice Item`
-				where parent=`tabSales Invoice`.name
+				where parent=i.name
 			"""
 		for dimension in accounting_dimensions:
 			if filters.get(dimension.fieldname):
@@ -444,7 +522,7 @@ def get_invoices(filters, additional_query_columns):
 		additional_query_columns = ', ' + ', '.join(additional_query_columns)
 
 	conditions = get_conditions(filters)
-	return frappe.db.sql("""
+	result =  frappe.db.sql("""
 		select st.sales_person,i.name, i.posting_date, i.customer,i.pb_sales_employee_name,i.is_return,i.return_against,
 		i.customer_name, i.owner,i.tax_id,
 
@@ -459,6 +537,52 @@ def get_invoices(filters, additional_query_columns):
 		left join `tabSales Team` st on st.parent = i.name
 		where i.docstatus = 1 %s order by i.posting_date desc, i.name desc""".format(additional_query_columns or '') %
 		conditions, filters, as_dict=1)
+	# mop = _get_mop() 
+	# if filters.get('summary_view'):
+	# 	result = _summarize_payments(
+	# 		groupby('sales_person', result),
+	# 		mop
+	# 	)
+	return result
+
+def _get_mop():
+	mop = frappe.get_all('POS Bahrain Settings MOP', fields=['mode_of_payment'])
+
+	if not mop:
+		frappe.throw(_('Please set Report MOP under POS Bahrain Settings'))
+
+	return list(pluck('mode_of_payment', mop))
+
+def _summarize_payments(result, mop):
+	summary = []
+
+	mop_cols = [
+		mop_col.replace(" ", "_").lower()
+		for mop_col in mop
+	]
+
+	def make_summary_row(_, row):
+		for col in mop_cols:
+			_[col] = _[col] + row[col]
+
+		_['posting_time'] = None
+		_['invoice'] = None
+
+		return _
+
+	for key, payments in result.items():
+		summary.append(
+			reduce(make_summary_row, payments)
+		)
+
+	get_row_total = compose(
+		sum, lambda x: x.values(), partial(keyfilter, lambda x: x in mop_cols)
+	)
+
+	return [merge(row, {'total': get_row_total(row)}) for row in summary]
+
+
+
 
 
 def get_invoice_income_map(invoice_list):
