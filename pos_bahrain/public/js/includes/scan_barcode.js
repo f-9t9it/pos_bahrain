049b5d090eb06bb4812ae4587132a148ba1c97c3
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-06-19 17:12:05 +0200

    Update scan_barcode.js

--- pos_bahrain/public/js/includes/scan_barcode.js
+++ pos_bahrain/public/js/includes/scan_barcode.js
@@ -1,6 +1,8 @@
 // Copyright (c) 2020, 	9t9it and contributors
 // For license information, please see license.txt
 
+frappe.provide('pos_bahrain.scripts.extensions');
+
 ['Stock Entry', 'Sales Invoice', 'Purchase Invoice'].forEach(doctype => {
   frappe.ui.form.off(doctype, 'scan_barcode');
   frappe.ui.form.on(
