7b36fc6e526da6475e07fa83beaf7fae2e2bf825
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-08-14 13:13:41 +0200

    Update gl_payment.js
    Update JS file to use frappe.provide to import js code in another file.

--- pos_bahrain/pos_bahrain/doctype/gl_payment/gl_payment.js
+++ pos_bahrain/pos_bahrain/doctype/gl_payment/gl_payment.js
@@ -1,7 +1,8 @@
 // Copyright (c) 2020, 	9t9it and contributors
 // For license information, please see license.txt
+frappe.provide("pos_bahrain.scripts.gl_payment")
 
-const script = pos_bahrain.scripts.gl_payment();
+const script = pos_bahrain.scripts.gl_payment;
 
 frappe.ui.form.on('GL Payment', script);
 frappe.ui.form.on('GL Payment Item', script.gl_payment_item);
