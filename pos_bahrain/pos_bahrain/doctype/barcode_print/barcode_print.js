3869d87be6d996d68bbd512e6c191e6336ef5247
Doreen2002 <doreenmwapekatebe8@gmail.com>
2024-09-14 12:59:55 +0200

    fixed js issues on loading files

--- pos_bahrain/pos_bahrain/doctype/barcode_print/barcode_print.js
+++ pos_bahrain/pos_bahrain/doctype/barcode_print/barcode_print.js
@@ -1,6 +1,9 @@
 // Copyright (c) 2019, 	9t9it and contributors
 // For license information, please see license.txt
 
+frappe.provide('pos_bahrain.scripts.barcode_print')
+frappe.provide('pos_bahrain.scripts.barcode_print.barcode_print_item')
+
 frappe.ui.form.on('Barcode Print', pos_bahrain.scripts.barcode_print);
 frappe.ui.form.on(
   'Barcode Print Item',
