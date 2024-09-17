263c8385b5616640335e4a260046fc16092c04e6
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-09-17 17:06:56 +0200

    updated js with provides to load stock child table js

--- pos_bahrain/public/js/stock_entry.js
+++ pos_bahrain/public/js/stock_entry.js
@@ -1,5 +1,6 @@
 // Copyright (c) 2018, 	9t9it and contributors
 // For license information, please see license.txt
+frappe.provide("pos_bahrain.scripts.extensions.stock_entry_item_code")
 
 frappe.ui.form.off('Stock Entry Detail', 'item_code');
 frappe.ui.form.on('Stock Entry Detail', {
