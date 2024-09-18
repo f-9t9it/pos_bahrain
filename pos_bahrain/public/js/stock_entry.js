a855e043f6610e54ae0c414a819ee6cad4c14bec
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-09-18 06:55:29 +0200

    Update stock_entry.js

--- pos_bahrain/public/js/stock_entry.js
+++ pos_bahrain/public/js/stock_entry.js
@@ -1,10 +1,12 @@
 // Copyright (c) 2018, 	9t9it and contributors
 // For license information, please see license.txt
-frappe.provide("pos_bahrain.scripts.extensions.stock_entry_item_code")
+
 
 frappe.ui.form.off('Stock Entry Detail', 'item_code');
 frappe.ui.form.on('Stock Entry Detail', {
-  item_code: pos_bahrain.scripts.extensions.stock_entry_item_code,
+  item_code: function(frm, cdt, cdn){
+    stock_entry_item_code(frm, cdt, cdn)
+  },
   s_warehouse: function (frm, cdt, cdn) {
     _set_cost_center('s_warehouse', cdt, cdn);
   },
@@ -54,3 +56,65 @@ function _set_repack_warehouses_read_only(frm) {
 // 		});
 //   },
 // }) 
+
+function stock_entry_item_code(frm, cdt, cdn)
+{
+  const d = frappe.get_doc(cdt, cdn);
+  if (d.item_code) {
+    // exists=frappe.db.get_single_value('POS Bahrain Settings', 'disable_serial_no_and_batch_selector')
+    // if (!exists){
+    const { company, doctype: voucher_type } = frm.doc;
+    const {
+      item_code,
+      s_warehouse,
+      t_warehouse,
+      transfer_qty,
+      serial_no,
+      bom_no,
+      expense_account,
+      cost_center,
+      qty,
+      name: voucher_no,
+    } = d;
+    frappe.call({
+      doc: frm.doc,
+      method: 'get_item_details',
+      callback: function(r) {
+        if (r.message) {
+          Object.keys(r.message).forEach(k => {
+            if (r.message[k]) {
+              d[k] = r.message[k];
+            }
+          });
+          frm.refresh_field('items');
+          return frappe.db.get_single_value('POS Bahrain Settings', 'disable_serial_no_and_batch_selector')
+                          .then((value) => {
+                            if (!value) {
+                              erpnext.stock.select_batch_and_serial_no(frm, d);
+                            }
+                          });
+                    
+          
+        }
+    },
+    error: function(r) {console.log(r)},
+    freeze: true,
+    async: true,
+      args: {
+        item_code,
+        warehouse: cstr(s_warehouse) || cstr(t_warehouse),
+        transfer_qty,
+        serial_no,
+        bom_no,
+        expense_account,
+        cost_center,
+        company,
+        qty,
+        voucher_type,
+        voucher_no,
+        allow_zero_valuation: 1,
+      },
+    });
+   
+  }
+}
