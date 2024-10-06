84e99ef8e5d17134b3c2d2bec2440b373dc3f992
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2024-10-06 13:57:01 +0200

    Added company and warehouse multiselect filter

--- pos_bahrain/pos_bahrain/report/daily_sales_summary/daily_sales_summary.py
+++ pos_bahrain/pos_bahrain/report/daily_sales_summary/daily_sales_summary.py
@@ -50,6 +50,7 @@ def _get_columns(filters):
 def _get_filters(filters):
     clauses = [
         "s.docstatus = 1",
+         "s.set_warehouse IN %(warehouse)s",
         "s.posting_date BETWEEN %(from_date)s AND %(to_date)s",
     ]
     return " AND ".join(clauses), filters
