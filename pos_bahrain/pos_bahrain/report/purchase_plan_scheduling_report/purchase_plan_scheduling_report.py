507f570f7e0466686384e767a9aa273c02ddebd4
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2025-02-25 17:03:19 +0200

    Corrected Expected Order QTY calculation

--- pos_bahrain/pos_bahrain/report/purchase_plan_scheduling_report/purchase_plan_scheduling_report.py
+++ pos_bahrain/pos_bahrain/report/purchase_plan_scheduling_report/purchase_plan_scheduling_report.py
@@ -241,7 +241,7 @@ def get_data(filters):
 			# if delivery_total_sales != []:
 			# 	for delivery in delivery_total_sales:
 			# 		total_sales_d += -(delivery.actual_qty)
-			expected_sales = total_sales + reorder_quantity + (total_sales * float(filters.percentage)/ 100)
+			expected_sales = total_sales + (total_sales * float(filters.percentage)/ 100)
 			total_months_in_report =  date_diff(filters.end_date , filters.start_date) / 30 if date_diff(filters.end_date , filters.start_date)>=30 else 0
 			monthly_sales = int(expected_sales) / int(total_months_in_report) if total_months_in_report != 0 else 0
 			annual_sales = monthly_sales * 12
@@ -249,7 +249,7 @@ def get_data(filters):
 			shortage_happened = (available_qty + on_purchase) -period_expected_sales 
 			min = monthly_sales * float(filters.minimum_months)
 			minimum_purchase_qty = get_minimum_purchase_qty(item.item_code, frappe.defaults.get_user_default("Company"))
-			expected_order_quantity = shortage_happened - min - min - minimum_purchase_qty
+			expected_order_quantity = total_sales + reorder_quantity + (total_sales * float(filters.percentage)/ 100)
 			priority_month = (available_qty + on_purchase) / monthly_sales if monthly_sales > 0 else 0
 			if filters.get('long_meter'):
 				long_meter_to_roll = expected_order_quantity / filters.get('long_meter')
