95fd1b2f3b5c4bda4f939bb9f05d8b87e75515c9
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2025-02-26 14:36:07 +0200

    Added Sales Report Summary

---
+++ pos_bahrain/pos_bahrain/report/sales_report_summary/sales_report_summary.js
@@ -0,0 +1,36 @@
+// Copyright (c) 2016, 	9t9it and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Sales Report Summary"] = {
+	"filters": [
+		{
+			"fieldname": "start_date",
+			"label": __("Start Date"),
+			"fieldtype": "Date",
+			"reqd": 1,
+			"default": frappe.datetime.get_today(),
+					},
+					{
+			"fieldname": "end_date",
+			"label": __("End Date"),
+			"fieldtype": "Date",
+			"reqd": 1,
+			"default": frappe.datetime.get_today(),
+					},
+				{
+					"fieldname": "company",
+					"label": __("Company"),
+					"fieldtype": "Link",
+					"reqd": 1,
+					"options":"Company",
+					"default":frappe.defaults.get_user_default("Company")
+							},
+							{
+					"fieldname": "warehouse",
+					"label": __("Warehouse"),
+					"fieldtype": "Link",
+					"options":"Warehouse"
+							},
+	]
+};
