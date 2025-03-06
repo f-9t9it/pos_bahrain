36b26a9948c779471cb5ef5fab73fc497f69dcd1
Doreen2002 <doreenmwapekatebe8@gmail.com>
2025-03-06 14:01:27 +0300

    item doctype, has batch no field enable/disable current stock wise

--- pos_bahrain/core_overrides/item.py
+++ pos_bahrain/core_overrides/item.py
@@ -1,5 +1,8 @@
 import frappe
 from erpnext.stock.doctype.item.item import Item
+from frappe import _, msgprint
+from frappe.utils import (cint, cstr, flt, formatdate, get_timestamp, getdate,
+						  now_datetime, random_string, strip, get_link_to_form)
 
 def custom_onload(self):
 		# super(Item, self).onload()
@@ -15,4 +18,24 @@ def custom_stock_ledger_created(self):
 
     return 1 if qty > 0 else 0 
 
+
+def custom_cant_change(self):
+    frappe.log_error(title="Custom custom_cant_change",message= "Custom custom_cant_change")
+    if not self.get("__islocal"):
+        fields = ("has_serial_no", "is_stock_item", "valuation_method", "has_batch_no")
+
+        values = frappe.db.get_value("Item", self.name, fields, as_dict=True)
+        if not values.get('valuation_method') and self.get('valuation_method'):
+            values['valuation_method'] = frappe.db.get_single_value("Stock Settings", "valuation_method") or "FIFO"
+
+        if values:
+            for field in fields:
+                if cstr(self.get(field)) != cstr(values.get(field)):
+                    if not self.check_if_linked_document_exists(field):
+                        break # no linked document, allowed
+                    # else:
+                    # 	frappe.throw(_("As there are existing transactions against item {0}, you can not change the value of {1}").format(self.name, frappe.bold(self.meta.get_label(field))))
+
 Item.onload = custom_onload
+Item.cant_change = custom_cant_change
+Item.stock_ledger_created = custom_stock_ledger_created
