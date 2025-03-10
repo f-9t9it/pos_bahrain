fcf3f32037b70bd8aa1900e633573dfe4600213a
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2025-03-10 16:40:00 +0200

    Update and rename sales_report_summary.js to product_purchase_summary.js

--- pos_bahrain/pos_bahrain/report/sales_report_summary/product_purchase_summary.js
+++ pos_bahrain/pos_bahrain/report/sales_report_summary/product_purchase_summary.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 /* eslint-disable */
 
-frappe.query_reports["Sales Report Summary"] = {
+frappe.query_reports["Product Purchase Summary"] = {
 	"filters": [
 		{
 			"fieldname": "start_date",
