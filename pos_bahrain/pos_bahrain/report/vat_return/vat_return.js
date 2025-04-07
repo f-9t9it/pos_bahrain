3b2c5f0c127aa55730025c6f05d24d4c4fd0c0f0
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2025-04-07 15:14:57 +0200

    Update vat_return.js

--- pos_bahrain/pos_bahrain/report/vat_return/vat_return.js
+++ pos_bahrain/pos_bahrain/report/vat_return/vat_return.js
@@ -2,4 +2,21 @@
 // For license information, please see license.txt
 /* eslint-disable */
 
-frappe.query_reports['VAT Return'] = pos_bahrain.reports.vat_return();
+frappe.provide('pos_bahrain.reports')
+
+frappe.query_reports['VAT Return'] = 
+{
+    filters : pos_bahrain.reports.vat_return,
+    formatter: function (value, row, column, data, default_formatter) {
+        const formatted = default_formatter(value, row, column, data);
+        if (data.bold) {
+          return $(`<span>${formatted}</span>`)
+            .css('font-weight', 'bold')
+            .wrap('<p />')
+            .parent()
+            .html();
+        }
+        return formatted;
+      },
+}
+
