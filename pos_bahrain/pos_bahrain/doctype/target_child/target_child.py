91ff373af378925e9f77d1a462404880811d8fb4
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2025-07-09 14:59:49 +0200

    removed target child table

--- pos_bahrain/pos_bahrain/doctype/target_child/target_child.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2025, 	9t9it and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class TargetChild(Document):
-	pass
