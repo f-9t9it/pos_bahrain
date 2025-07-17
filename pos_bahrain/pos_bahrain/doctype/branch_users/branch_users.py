b09d1e0881103dffedcda423b559c873c25a6992
Doreen2002 <doreenmwapekatebe8@gmail.com>
2025-07-17 11:32:29 +0200

    child table- branch users

---
+++ pos_bahrain/pos_bahrain/doctype/branch_users/branch_users.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2025, 	9t9it and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class BranchUsers(Document):
+	pass
