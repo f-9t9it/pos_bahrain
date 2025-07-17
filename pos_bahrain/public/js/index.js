fd5dc054b8e6cea29ee96b3c4b0fa50ba01c3efd
MwapeDoreen <50717922+Doreen2002@users.noreply.github.com>
2025-07-17 11:51:09 +0200

    workflow configuration

--- pos_bahrain/public/js/index.js
+++ pos_bahrain/public/js/index.js
@@ -46,7 +46,6 @@ frappe.ui.form.on(
   scripts.payment_entry.payment_entry_reference
 );
 
-frappe.ui.form.on('Stock Transfer Item', scripts.stock_transfer_item);
-frappe.ui.form.on('Stock Transfer', scripts.stock_transfer);
+
 
 pos_bahrain = { scripts, reports, addons };
