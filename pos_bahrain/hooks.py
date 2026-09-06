bd6e9b37368c92d979c597b4b091c17e10e9d4bc
Doreen2002 <doreenmmwapekatebe8@gmail.com>
2026-09-06 10:50:00 +0300

    Add - Delivery Note-custom_branch field

--- pos_bahrain/hooks.py
+++ pos_bahrain/hooks.py
@@ -284,7 +284,8 @@ fixtures = [
 					"Stock Entry-custom_total_qty",
 					"Stock Entry-custom_difference_account_",
 					"Stock Entry-custom_difference_acount",
-                    "Account-custom_account_name_arabic"
+                    "Account-custom_account_name_arabic",
+                    "Delivery Note-custom_branch"
                 ],
             ],
         ],
