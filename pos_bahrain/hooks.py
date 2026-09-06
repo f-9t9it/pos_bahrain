0f6e1190b60ae53ed46e92e0cfc10f6c29944f27
Doreen2002 <doreenmmwapekatebe8@gmail.com>
2026-09-06 10:28:27 +0300

    Remove - Purchase Order-custom_country, Sales Order-custom_country, Purchase Order-custom_region custom fields

--- pos_bahrain/hooks.py
+++ pos_bahrain/hooks.py
@@ -284,10 +284,7 @@ fixtures = [
 					"Stock Entry-custom_total_qty",
 					"Stock Entry-custom_difference_account_",
 					"Stock Entry-custom_difference_acount",
-                    "Account-custom_account_name_arabic",
-					"Purchase Order-custom_country",
-					"Sales Order-custom_country",
-					"Purchase Order-custom_region"
+                    "Account-custom_account_name_arabic"
                 ],
             ],
         ],
