160549093c5c8b280d9a051b35443216beccc699
Doreen2002 <doreenmmwapekatebe8@gmail.com>
2026-09-10 14:19:05 +0300

    Remove - sales invoice create button

--- pos_bahrain/public/js/quotation.js
+++ pos_bahrain/public/js/quotation.js
@@ -1,7 +1,6 @@
 frappe.ui.form.on('Quotation', {
   refresh: function (frm) {
     get_employee(frm);
-    _create_custom_buttons(frm);
     query_override(frm);
   },
   quotation_to: function(frm) {
@@ -36,30 +35,6 @@ frappe.ui.form.on("Quotation", {
 	
 })
 
-function _create_custom_buttons(frm) {
-  if (frm.doc.docstatus === 1 && frm.doc.status !== 'Lost') {
-    if (
-      !frm.doc.valid_till ||
-      frappe.datetime.get_diff(
-        frm.doc.valid_till,
-        frappe.datetime.get_today()
-      ) >= 0
-    ) {
-      frm.add_custom_button(
-        __('Sales Invoice'),
-        () => _make_sales_invoice(frm),
-        __('Create')
-      );
-    }
-  }
-}
-
-function _make_sales_invoice(frm) {
-  frappe.model.open_mapped_doc({
-    method: 'pos_bahrain.api.quotation.make_sales_invoice',
-    frm,
-  });
-}
 
 function query_override(frm){
   if(cur_frm.doc.quotation_to == "Customer"){
