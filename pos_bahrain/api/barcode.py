6c5996629deb80bc2d4bba7f176f03481791cbd6
Doreen2002 <doreenmmwapekatebe8@gmail.com>
2026-09-16 15:41:26 +0300

    Define barcode width and height

--- pos_bahrain/api/barcode.py
+++ pos_bahrain/api/barcode.py
@@ -4,11 +4,14 @@ import barcode
 from barcode.writer import SVGWriter
 
 @frappe.whitelist()
-def generate_barcode_svg(data: str, code_type: str = "code128") -> str:
+def generate_barcode_svg(data: str, code_type: str = "code128", height: float = 10, width: float = 0.2) -> str:
     barcode_class = barcode.get_barcode_class(code_type)
     byte_stream = io.BytesIO()
     barcode_instance = barcode_class(data, writer=SVGWriter())
-    barcode_instance.write(byte_stream)
+    barcode_instance.write(byte_stream,options={
+        "module_width": width,
+        "module_height": height,
+    })
     svg_string = byte_stream.getvalue().decode("utf-8")
     if "<?xml" in svg_string:
         svg_string = svg_string.split("?>", 1)[-1].strip()
